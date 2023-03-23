5e75f2b77b85679c064b480f9c9e7fc5e3732b17
rehan2912 <rehanpathan29122000@gmail.com>
2023-03-24 00:36:47 +0530

    routes changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { addUser, login, addAddress, getBankDetails, getAddress, updateWishlist } from "../controllers/users.js";
+import { addUser, login, addAddress, getBankDetails, getAddress, updateWishlist, addBankDetails } from "../controllers/users.js";
 
 const router = express.Router();
 
 /* READ */
 router.post("/register", addUser);
 router.post("/login", login);
-router.post("/:uid/address", addAddress);
 router.get("/:uid/bankDetails", getBankDetails);
+router.post("/:uid/bankDetails", addBankDetails);
 router.post("/:uid/address", addAddress);
 router.get("/:uid/address", getAddress);
 router.patch("/:uid/wishlist", updateWishlist);
